2fb59ddcd799686beb4c92aa1457592445c44f4a
Divyaditya Singh <divyadityasolanki20570@acropolis.in>
2025-01-23 14:33:56 +0530

    get-deleted module created

--- src/main/java/com/example/demo/service/CategoryService.java
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -13,4 +13,8 @@ public interface CategoryService {
     public List<CategoryDto> getAllCategory();
 
     public List<CategoryResponseDto> getActiveCategories();
+
+    public CategoryDto getCategoryById(Integer id);
+
+    public Boolean deleteCategoryById(Integer id);
 }
--- src/main/java/com/example/demo/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/serviceImpl/CategoryServiceImpl.java
@@ -2,11 +2,13 @@ package com.example.demo.serviceImpl;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+import org.w3c.dom.CDATASection;
 
 import com.example.demo.categoryDto.CategoryDto;
 import com.example.demo.categoryDto.CategoryResponseDto;
@@ -43,7 +45,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<CategoryDto> getAllCategory() {
-        List<Category> categories = categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findByIsDeletedFalse();
         List<CategoryDto> categoryDtoList = categories.stream().map(cat -> modelMapper.map(cat, CategoryDto.class))
                 .toList();
         return categoryDtoList;
@@ -51,11 +53,32 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<CategoryResponseDto> getActiveCategories() {
-        List<Category> categories = categoryRepository.findByIsActiveTrue();
+        List<Category> categories = categoryRepository.findByIsActiveTrueAndIsDeletedFalse();
         List<CategoryResponseDto> categoryResponseDtoList = categories.stream()
                 .map(cat -> modelMapper.map(cat, CategoryResponseDto.class)).toList();
         return categoryResponseDtoList;
 
+    }
+
+    public CategoryDto getCategoryById(Integer id) {
+        Optional<Category> category = categoryRepository.findByIdAndIsDeletedFalse(id);
+        if (category.isPresent()) {
+            Category category2 = category.get();
+            return modelMapper.map(category2, CategoryDto.class);
+        }
+        return null;
+    }
+
+    @Override
+    public Boolean deleteCategoryById(Integer id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        if (category.isPresent()) {
+            Category category2 = category.get();
+            category2.setIsDeleted(true);
+            categoryRepository.save(category2);
+            return true;
+        }
+        return false;
     }
 
 }
